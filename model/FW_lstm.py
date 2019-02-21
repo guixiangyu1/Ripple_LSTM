a3af9fde0e559c7bccf3f0a993ae1114862b486a
guixiangyu <guixiangyu1@gmail.com>
2019-02-21 22:14:36 +0800

    test initial_state , user-define LSTMStateTuple

--- model/FW_lstm.py
+++ model/FW_lstm.py
@@ -20,7 +20,18 @@ class FW_lstm():
     #     outputs.append(output)
     # return (outputs, state)
     def push(self, input):
-        _output = tf.nn.dynamic_rnn(
+        output = tf.nn.dynamic_rnn(
             self.cell_fw, input, initial_state= self.state,
             sequence_length=self.word_length, dtype=tf.float32)
-        output =
+        output, (self.state) = output
+
+    # 测试总结：
+    # 1. dynamic_rnn的输出output=（ndarray,LSTMStateTuple）
+    # 2. ndarray代表了所有的输出，为[nbatch, nstep, nhidden], dynamic_rnn的输入其实都已经补齐了，因此输出也是整齐的，
+    #    只不过输出中一大堆没用的部分，这些没用的部分最终是用mask抹去
+    # 3. LSTMStateTuple (c,h); c为cell状态，h为末端最后输出；二者均为最后一个lstmcell的状态
+    # 4. sequence_length = 0时, 对应的输出为0，但是对应的状态为输入的状态不变
+
+######################测试目标：sequence_length 为0时，output的输出是什么？######
+    # tf.contrib.rnn.LSTMStateTuple()
+    # 意外的结果：输出全部为0，状态完全由initia_state决定，与之相同
